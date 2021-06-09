e47ccd9e1c711ab426d1165d04e8311180135343
Zixian Cai <caizixian@users.noreply.github.com>
2021-06-09 22:36:24 +1000

    Add metadata to Note wrapper type

--- src/note.rs
+++ src/note.rs
@@ -1,14 +1,21 @@
 use crate::notetype::NoteType;
+use std::time::SystemTime;
 
 pub(super) type NoteID = String;
 pub(super) type Tag = String;
 
 pub struct Note<T: NoteType> {
-    note: T
+    note: T,
+    created_at: SystemTime,
+    modified_at: SystemTime,
 }
 
 impl<T: NoteType> Note<T> {
-    pub fn new(note: T) {
-        Note { note };
+    pub fn new(note: T) -> Self {
+        Note {
+            note,
+            created_at: SystemTime::now(),
+            modified_at: SystemTime::now(),
+        }
     }
 }
