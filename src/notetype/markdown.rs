0d551ec93a5b37d936c682a7f86e5b8d34ea7c9b
Zixian Cai <caizixian@users.noreply.github.com>
2021-06-11 00:06:51 +1000

    Add scaffolding for Markdown support

--- src/notetype/markdown.rs
+++ src/notetype/markdown.rs
@@ -1,10 +1,15 @@
 use crate::{NoteID, NoteType, Tag};
+use pulldown_cmark::{Options, Parser};
+use pulldown_cmark_to_cmark::cmark;
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub enum MarkdownNoteError {}
+pub enum MarkdownNoteError {
+    #[error("format error")]
+    FormatError(#[from] std::fmt::Error),
+}
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone, PartialEq, Eq)]
 pub struct MarkdownNote {
@@ -41,6 +46,22 @@ impl NoteType for MarkdownNote {
         _old_referent: NoteID,
         _new_referent: NoteID,
     ) -> Result<(), Self::Error> {
-        todo!()
+        let mut options = Options::empty();
+        options.insert(Options::ENABLE_TABLES);
+        options.insert(Options::ENABLE_FOOTNOTES);
+        options.insert(Options::ENABLE_STRIKETHROUGH);
+        options.insert(Options::ENABLE_TASKLISTS);
+        options.insert(Options::ENABLE_SMART_PUNCTUATION);
+        let mut buf = String::new();
+        let parser = Parser::new_ext(&self.body, options).map(|event| match event {
+            _ => event,
+        });
+        match cmark(parser, &mut buf, None) {
+            Ok(_) => {
+                self.body = buf;
+                Ok(())
+            }
+            Err(e) => Err(MarkdownNoteError::FormatError(e)),
+        }
     }
 }
