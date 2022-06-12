39158d3b70932a5ecbc8cd5d3875f5db9d30ad2b
Zixian Cai <caizixian@users.noreply.github.com>
2022-06-12 23:22:06 +1000

    Implement tags

--- notegraf/src/notemetadata.rs
+++ notegraf/src/notemetadata.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 use std::time::SystemTime;
 
 pub static NOTE_METADATA_CURRENT_SCHEMA_VERSION: u64 = 0;
@@ -8,6 +9,7 @@ pub struct NoteMetadata {
     pub schema_version: u64,
     pub created_at: SystemTime,
     pub modified_at: SystemTime,
+    pub tags: HashSet<String>,
 }
 
 impl Default for NoteMetadata {
@@ -17,6 +19,7 @@ impl Default for NoteMetadata {
             schema_version: NOTE_METADATA_CURRENT_SCHEMA_VERSION,
             created_at: now,
             modified_at: now,
+            tags: HashSet::new(),
         }
     }
 }
@@ -29,6 +32,53 @@ impl NoteMetadata {
             schema_version: NOTE_METADATA_CURRENT_SCHEMA_VERSION,
             created_at: self.created_at,
             modified_at: SystemTime::now(),
+            tags: self.tags.clone(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{InMemoryStore, NoteStore, PlainNote};
+
+    #[tokio::test]
+    async fn update_note_tags() {
+        let store: InMemoryStore<PlainNote> = InMemoryStore::new();
+        let loc1 = store.new_note(PlainNote::new("Foo".into())).await.unwrap();
+        let rev1 = loc1.get_revision().unwrap();
+        let metadata1 = store
+            .get_note(&loc1.current())
+            .await
+            .unwrap()
+            .get_metadata();
+        let mut new_metadata = metadata1.clone();
+        new_metadata.tags.insert("my_tag".to_owned());
+        let loc2 = store
+            .update_note(&loc1, None, Some(new_metadata))
+            .await
+            .unwrap();
+        let rev2 = loc2.get_revision().unwrap();
+        assert_ne!(rev1, rev2);
+        assert!(!store
+            .get_note(&loc1)
+            .await
+            .unwrap()
+            .get_metadata()
+            .tags
+            .contains("my_tag"));
+        assert!(store
+            .get_note(&loc1.at_revision(rev2))
+            .await
+            .unwrap()
+            .get_metadata()
+            .tags
+            .contains("my_tag"));
+        assert!(store
+            .get_note(&loc1.current())
+            .await
+            .unwrap()
+            .get_metadata()
+            .tags
+            .contains("my_tag"));
     }
 }
