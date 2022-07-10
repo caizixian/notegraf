b8ee97f827a2b0758d1294d8ca433a73ce353d1e
Zixian Cai <2891235+caizixian@users.noreply.github.com>
2022-07-10 23:14:45 +1000

    Fix code block coloring (#93)

--- notegraf-web/tailwind.config.js
+++ notegraf-web/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
     content: ["./src/frontend/**/*.{html,js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            typography: {
+                github: {
+                    css: {
+                        '--tw-prose-pre-code': '#333',
+                        '--tw-prose-pre-bg': '#fff',
+                        '--tw-prose-invert-pre-code': '#c9d1d9',
+                        '--tw-prose-invert-pre-bg': '#0d1117',
+                    },
+                },
+            }
+        },
     },
     variants: {},
     plugins: [
