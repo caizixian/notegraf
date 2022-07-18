0cd74e34727ba2f86a8a25e2be700f9409f9a434
Zixian Cai <2891235+caizixian@users.noreply.github.com>
2022-07-18 19:52:51 +1000

    Support SSO sign out (#120)

--- notegraf-web/src/frontend/pages/App.tsx
+++ notegraf-web/src/frontend/pages/App.tsx
@@ -1,24 +1,11 @@
-import {NavLink, Outlet} from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import * as React from "react";
-import SearchBox from "../components/SearchBox";
-import {DocumentAddIcon} from "@heroicons/react/outline";
+import {NavBar} from "../components/NavBar";
 
 export function App() {
     return (
         <div className={"min-h-screen bg-white dark:bg-slate-800 dark:text-white flex flex-col"}>
-            <nav className={"flex p-1 w-full bg-gray-500 items-center gap-1"}>
-                <NavLink to={"/note"}>Recent</NavLink>
-                <div className={"ml-auto flex gap-1 items-center min-w-0"}>
-                    <div className={"min-w-0"}>
-                        <SearchBox/>
-                    </div>
-                    <NavLink to={"/note/new"}>
-                        <button className={"ng-button ng-button-primary"}>
-                            <DocumentAddIcon className={"h-6 w-6"}/>
-                        </button>
-                    </NavLink>
-                </div>
-            </nav>
+            <NavBar/>
             <Outlet/>
         </div>
     );
