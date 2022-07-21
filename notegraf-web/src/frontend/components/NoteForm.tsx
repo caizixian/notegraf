b252ce11ac1d4fae34adc00f191a626851a38af4
Zixian Cai <2891235+caizixian@users.noreply.github.com>
2022-07-21 23:06:19 +1000

    Support previewing while editing

--- notegraf-web/src/frontend/components/NoteForm.tsx
+++ notegraf-web/src/frontend/components/NoteForm.tsx
@@ -4,6 +4,8 @@ import {incrementCounter, useLocalStorage} from "../utils";
 import {postNote} from "../api";
 import * as React from "react";
 import {useEffect, useState} from "react";
+import {RenderMarkdown} from "./Note";
+import {CodeIcon, DocumentTextIcon} from "@heroicons/react/outline";
 
 type NoteFormContent = {
     title: string,
@@ -31,9 +33,10 @@ function isValidJSON(s: string): boolean {
 
 
 export function NoteForm(props: NoteFormProps) {
-    const {register, watch, setValue, handleSubmit} = useForm();
+    const {register, watch, setValue, handleSubmit, getValues} = useForm();
     const navigate = useNavigate();
     const [error, setError] = useState<any>(null);
+    const [preview, setPreview] = useState(false);
     useLocalStorage(props.autoSaveKey, watch, setValue, props.defaultValue, 5000);
 
     const onSubmit = async (data: any) => {
@@ -67,15 +70,30 @@ export function NoteForm(props: NoteFormProps) {
                 <label htmlFor={"tags"}>Tags</label>
                 <input className={"form-input bg-transparent w-full"} id={"tags"} placeholder={"comma separated"}
                        type={"text"} {...register("metadata_tags")} />
+                <button className={"ng-button ng-button-primary"}
+                        title={preview ? "Show source" : "Preview"}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setPreview(!preview);
+                        }}>
+                    {preview ?
+                        (<CodeIcon className={"h-6 w-6"}/>) :
+                        (<DocumentTextIcon className={"h-6 w-6"}/>)
+                    }
+                </button>
             </div>
             <div className={"flex gap-2 m-1 items-center"}>
                 <label htmlFor={"custom_metadata"}>Custom metadata</label>
                 <input className={"form-input bg-transparent w-full"} id={"custom_metadata"} placeholder={"{}"}
                        type={"text"} {...register("metadata_custom_metadata", {validate: isValidJSON})} />
             </div>
-            <div className={"flex-1 flex"}>
+            <div className={"flex-1 flex justify-center"}>
                 <textarea required={true} autoFocus={true} id={"note_inner"}
-                          className={"form-textarea bg-transparent flex-1 font-mono"} {...register("note_inner")}></textarea>
+                          className={"form-textarea bg-transparent flex-1 font-mono"}
+                          hidden={preview}
+                          spellCheck={true}
+                          {...register("note_inner")}></textarea>
+                {preview && <RenderMarkdown note_inner={getValues("note_inner") || ""}/>}
             </div>
         </form>
     );
