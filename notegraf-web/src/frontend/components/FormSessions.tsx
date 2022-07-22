e5c8ac8a9ee0ad3bb004a304f5dabbba4663ae3b
Zixian Cai <2891235+caizixian@users.noreply.github.com>
2022-07-22 23:42:53 +1000

    Allow editing session to be deleted

--- notegraf-web/src/frontend/components/FormSessions.tsx
+++ notegraf-web/src/frontend/components/FormSessions.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import {useEffect, useState} from "react";
-import {getKeysByPrefix, showAgo} from "../utils";
+import {getKeysByPrefix, getStorageValue, renderTitle, showAgo} from "../utils";
 import {Link, useNavigate} from "react-router-dom";
+import * as types from "../types";
+import {TrashIcon} from "@heroicons/react/outline";
 
 type FormSessionsProps = {
     keyPrefix: string,
@@ -11,26 +13,49 @@ type FormSessionsProps = {
 export function FormSessions(props: FormSessionsProps) {
     const navigate = useNavigate();
     const [keys, setKeys] = useState<string[]>([]);
+    const [notes, setNotes] = useState<types.Note[]>([]);
 
-    useEffect(() => {
-        document.title = props.title;
-        const existingKeys = getKeysByPrefix(props.keyPrefix);
+    function loadSessions() {
+        let existingKeys = getKeysByPrefix(props.keyPrefix);
+        existingKeys.sort();
+        let notes = existingKeys.map(key => getStorageValue(key, null));
         if (existingKeys.length == 0) {
             navigate(`./${Date.now()}`);
         } else {
             setKeys(existingKeys);
+            setNotes(notes);
         }
+    }
+
+    useEffect(() => {
+        document.title = props.title;
+        loadSessions();
     }, []);
     return (<div>
         Choose a session:
-        <ul>
-            {keys.map(key => {
+        <div>
+            {keys.map((key, idx) => {
                 let parts = key.split('.');
                 let ts = parts[parts.length - 1];
                 let date = new Date(parseInt(ts));
-                return (<li key={key}><Link to={`./${ts}`} className={"underline"}>{showAgo(date)}</Link></li>);
+                let note = notes[idx];
+                let title = note ? renderTitle(note.title) : (<span></span>);
+                return (<div key={key} className={"my-0.5"}>
+                    <Link to={`./${ts}`} className={"underline"}>{title} ({showAgo(date)})</Link>
+                    <button
+                        onClick={() => {
+                            if (window.confirm("Are you sure you want to delete this session?")) {
+                                localStorage.removeItem(key);
+                                loadSessions();
+                            }
+                        }}
+                        className={"ng-button ng-button-danger inline ml-2"}
+                        title={"Delete"}>
+                        <TrashIcon className={"h-4 w-4"}/>
+                    </button>
+                </div>);
             })}
-            <li><Link to={`./${Date.now()}`} className={"underline"}>New session</Link></li>
-        </ul>
+            <div className={"my-0.5"}><Link to={`./${Date.now()}`} className={"underline"}>New session</Link></div>
+        </div>
     </div>);
 }
