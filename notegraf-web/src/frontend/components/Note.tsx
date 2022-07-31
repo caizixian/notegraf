6c36dedfbfac46509eac2a6758bb3a570a3c0e25
Zixian Cai <2891235+caizixian@users.noreply.github.com>
2022-07-31 15:07:15 +1000

    Add an icon to indicate external links

--- notegraf-web/src/frontend/components/Note.tsx
+++ notegraf-web/src/frontend/components/Note.tsx
@@ -75,11 +75,19 @@ const renderer = {
         if (href.indexOf("notegraf:") === 0) {
             href = href.slice(9);
         }
+        const isExternalURL = new URL(href, location.origin).origin !== location.origin;
         let out = '<a href="' + escapeHtml(href) + '"';
         if (title) {
             out += ' title="' + title + '"';
         }
-        out += '>' + text + '</a>';
+        out += '>';
+        out += text;
+        if (isExternalURL) {
+            out += '<svg xmlns="http://www.w3.org/2000/svg" class="h-[1em] w-[1em] inline" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">' +
+                '<path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />' +
+                '</svg>';
+        }
+        out += '</a>';
         return out;
     }
 }
--- notegraf-web/src/frontend/components/Tags.tsx
+++ notegraf-web/src/frontend/components/Tags.tsx
@@ -12,7 +12,7 @@ export function Tags(props: TagsProps) {
     tags.sort();
     return (<div className={"flex gap-1 flex-wrap"}>{props.tags.map(tag =>
         <div key={tag} className={"flex items-center rounded border border-neutral-500"}>
-            <TagIcon className={"h-4 w-4 inline"}/>
+            <TagIcon className={"h-[1em] w-[1em] inline"}/>
             {props.disableLink ? <p className={"select-none"}>{tag}</p> :
                 <Link to={"/note?" + new URLSearchParams({query: `#${tag}`})} className={"select-none"}>{tag}</Link>}
         </div>
